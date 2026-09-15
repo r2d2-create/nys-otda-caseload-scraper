4eb1f7c684071217e8f2fd56fd1598c19dde0e12
Nanda Molai Deopersaud <107003251+r2d2-create@users.noreply.github.com>
2026-09-14 22:04:55 -0400

    Corrected PDF URL

--- scrape_otda.py
+++ scrape_otda.py
@@ -25,7 +25,7 @@ with sync_playwright() as p:
     for year in years:
         for month in months:
             month_label = f"{year}-{month}"
-            pdf_url = f"https://ny.gov{year}/{month_label}-stats.pdf"
+            pdf_url = ("https://otda.ny.gov/resources/caseload/"f"{year}/{month_label}-stats.pdf")
 
             try:
                 response = page.goto(pdf_url, wait_until="networkidle")
